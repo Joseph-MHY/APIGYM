27b1c2eaa464d3b74e25994fac46b0a0d2318cfb
Joseph-MHY <huamaniyallij@gmail.com>
2024-04-08 20:51:45 -0500

    Actualizacion V0.1.1

--- src/main/java/pe/edu/idat/Controllers/ConfiguracionEntrenamientoController.java
+++ src/main/java/pe/edu/idat/Controllers/ConfiguracionEntrenamientoController.java
@@ -3,6 +3,7 @@ package pe.edu.idat.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.idat.DTO.ConfiguracionEntrenamientoDTO;
 import pe.edu.idat.Models.ConfiguracionEntrenamiento;
 import pe.edu.idat.Models.UsuarioConfiguracion;
 import pe.edu.idat.Models.Usuarios;
@@ -10,6 +11,7 @@ import pe.edu.idat.Services.ConfiguracionEntrenamientoService;
 import pe.edu.idat.Services.UsuarioService;
 import pe.edu.idat.Repositories.IUsuarioConfigurationRepo;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -19,53 +21,29 @@ import java.util.Optional;
 public class ConfiguracionEntrenamientoController {
 
     private final ConfiguracionEntrenamientoService configuracionEntrenamientoService;
-    private final UsuarioService usuarioService;
-    private final IUsuarioConfigurationRepo usuarioConfiguracionRepository;
 
     @Autowired
-    public ConfiguracionEntrenamientoController(ConfiguracionEntrenamientoService configuracionEntrenamientoService,
-                                                UsuarioService usuarioService,
-                                                IUsuarioConfigurationRepo usuarioConfiguracionRepository) {
+    public ConfiguracionEntrenamientoController(ConfiguracionEntrenamientoService configuracionEntrenamientoService) {
         this.configuracionEntrenamientoService = configuracionEntrenamientoService;
-        this.usuarioService = usuarioService;
-        this.usuarioConfiguracionRepository = usuarioConfiguracionRepository;
     }
 
-    @PostMapping("/POST")
-    public ResponseEntity<Object> postConfiguracionEntrenamiento(@RequestBody Map<String, Object> requestBody) {
+    @PostMapping("/configuracion")
+    public ResponseEntity<Object> crearConfiguracionEntrenamiento(@RequestBody ConfiguracionEntrenamientoDTO configuracionDTO) {
         try {
-            String correoUsuario = (String) requestBody.get("correoUsuario");
-            Map<String, Object> configuracionData = (Map<String, Object>) requestBody.get("configuracionEntrenamiento");
+            // Crear una nueva instancia de ConfiguracionEntrenamiento a partir de los datos recibidos en el DTO
+            ConfiguracionEntrenamiento nuevaConfiguracion = new ConfiguracionEntrenamiento();
+            nuevaConfiguracion.setDiasEntrenamiento(configuracionDTO.getDiasEntrenamiento());
+            nuevaConfiguracion.setDiaInicioEntrenamiento(configuracionDTO.getDiaInicioEntrenamiento());
 
-            if (correoUsuario == null || configuracionData == null) {
-                return ResponseEntity.badRequest().body("El correo del usuario o los datos de configuración de entrenamiento no pueden ser nulos.");
-            }
+            // Guardar la nueva configuración de entrenamiento utilizando el servicio
+            configuracionEntrenamientoService.guardarConfiguracion(nuevaConfiguracion);
 
-            Optional<Usuarios> usuarioOptional = usuarioService.getUsuarioByEmail(correoUsuario);
-            if (!usuarioOptional.isPresent()) {
-                return ResponseEntity.badRequest().body("No se encontró ningún usuario con el correo proporcionado.");
-            }
-
-            Usuarios usuario = usuarioOptional.get();
-
-            ConfiguracionEntrenamiento configuracionEntrenamiento = new ConfiguracionEntrenamiento();
-            configuracionEntrenamiento.setDiasEntrenamiento((Integer) configuracionData.get("diasEntrenamiento"));
-            configuracionEntrenamiento.setDiaInicioEntrenamiento((String) configuracionData.get("diaInicioEntrenamiento"));
-
-            ConfiguracionEntrenamiento configuracionGuardada = configuracionEntrenamientoService.guardarConfiguracion(configuracionEntrenamiento);
-
-            if (configuracionGuardada != null) {
-                UsuarioConfiguracion usuarioConfiguracion = new UsuarioConfiguracion();
-                usuarioConfiguracion.setUsuario(usuario);
-                usuarioConfiguracion.setConfiguracionEntrenamiento(configuracionGuardada);
-                usuarioConfiguracionRepository.save(usuarioConfiguracion);
-
-                return ResponseEntity.accepted().body("Configuración de entrenamiento creada correctamente y asociada al usuario.");
-            } else {
-                return ResponseEntity.badRequest().body("Error al guardar la configuración de entrenamiento.");
-            }
+            // Retornar una respuesta exitosa con el mensaje de éxito
+            Map<String, String> response = new HashMap<>();
+            response.put("mensaje", "Se ha insertado la configuración correctamente");
+            return ResponseEntity.ok(response);
         } catch (Exception ex) {
-            ex.printStackTrace();
+            // Manejar cualquier error y retornar una respuesta de error
             return ResponseEntity.badRequest().body("Error al procesar la solicitud: " + ex.getMessage());
         }
     }
